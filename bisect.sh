09f906c1661cd52991083b3523a9900ebd3ad06c
Jason Gross <jgross@mit.edu>
2020-04-29 21:48:59 -0400

    Bisect from older commit

--- bisect.sh
+++ bisect.sh
@@ -6,5 +6,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # git bisect start 3c0ba7afdf 18aa9ca60e && git bisect run ../coq-build.sh
 # c48af75d6c60119c826c4dbfd6ede053d3ee9181 is V8.11.1
 echo -n "::group::Starting bisect"
-git bisect start 9de39626d925aa8ed0138fc5b90ee30ccf1ac4c1 c48af75d6c60119c826c4dbfd6ede053d3ee9181 && git bisect run "${DIR}/coq-build.sh"
+git bisect start 9de39626d925aa8ed0138fc5b90ee30ccf1ac4c1 18aa9ca60e && git bisect run "${DIR}/coq-build.sh"
 echo "::endgroup::"
